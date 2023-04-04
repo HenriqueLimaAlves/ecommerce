47c2671d3147b47a87a94455405b5ca9004f79a7
HenriqueLimaAlves <henri633@gmail.com>
2023-04-03 23:50:58 -0300

    persistence.xml e add de dependencias

---
+++ src/main/java/estudo/model/Produto.java
@@ -0,0 +1,43 @@
+package estudo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+//@EqualsAndHashcode pode gerar problemas de performance, ler mais em: https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+public class Produto {
+
+    @Id
+    private Integer id;
+
+    private String nome;
+
+    private String descricao;
+
+    private BigDecimal preco;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Produto))
+            return false;
+
+        Produto other = (Produto) o;
+
+        return id != null &&
+                id.equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+}
